badf25108a9d9f5bc563a04cdb0523a0251c05fd
Unyek <uyamadani29@gmail.com>
2020-10-11 21:22:50 +0700

    first commit

---
+++ app/src/main/java/com/surya/myflexiblefragment/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.surya.myflexiblefragment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.surya.myflexiblefragment.R
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
